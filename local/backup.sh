1ad792c747cd481751b695a8101f67245e7d7da5
jeancdev <jcode2006@gmail.com>
2025-12-18 04:08:44 -0500

    Update backup script to create backups directory and include all world data in the archive

--- local/backup.sh
+++ local/backup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-tar -czf "backups/world-$TIMESTAMP.tar.gz" data/world
+
+mkdir -p backups
+
+tar -czf "backups/world-$TIMESTAMP.tar.gz" data/world data/world_nether data/world_the_end data/plugins
